4c198317a97bff0257ecf70bcb5d5fd833f20ac1
Atul Anand <45028800+atquil@users.noreply.github.com>
2022-08-21 15:28:54 +0530

    Introduction to Dart (#1)

---
+++ Dart Tutorial/1. Introduction.dart
@@ -0,0 +1,24 @@
+/// Entrypoint where app execution starts: main()
+/// 1. void : It will not return anything
+/// 2. main : main is the keyword,  () indicates it's a function definition, with {} indicating body of function
+/// 3. Arguments in main () - Optional.
+
+// * Without argument : When the control of program is not required.
+
+/*
+void main() {
+  print(" Hey we started writing code!!");
+}
+*/
+
+/*
+main() {
+  print("No return type, then it will be assumed as void, but use in this way. ");
+}
+*/
+
+// * With arguments : Here is list of different argument we can use https://pub.dev/packages/args
+
+void main(List<String> args) {
+  print("Pledge: We will code with you. ");
+}
