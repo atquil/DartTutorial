06215cc9bfff7a0fccfa2258635db2c267790ce4
aulanand <atulanandmishra@gamil.com>
2022-12-10 22:11:58 +0530

    Introduction

--- Introduction.dart
+++ Introduction.dart
@@ -1,24 +1,27 @@
-/// Entrypoint where app execution starts: main()
-/// 1. void : It will not return anything
-/// 2. main : main is the keyword,  () indicates it's a function definition, with {} indicating body of function
-/// 3. Arguments in main () - Optional.
-
-// * Without argument : When the control of program is not required.
-
+//Option 1 :
 /*
 void main() {
   print(" Hey we started writing code!!");
 }
 */
 
+// Option 2:
 /*
 main() {
   print("No return type, then it will be assumed as void, but use in this way. ");
 }
 */
 
+// Option 3:
 // * With arguments : Here is list of different argument we can use https://pub.dev/packages/args
 
 void main(List<String> args) {
   print("Pledge: We will code with you. ");
 }
+
+/*
+Program Explanation :
+1. This program has single function called main(), which is the entrypoint for the program.
+2. void keyword indicates that main() does not return any value
+3. Arguments inside i.e main(Argument), is required when you want to control the program : Not required for this course
+ */
